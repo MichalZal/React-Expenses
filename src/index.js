cbf4ce7e94e16301cb796d71573ae3b59bb14ff0
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-05-22 22:39:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
-// to jest w package.json i jest zainstalowane lokalnie. 
-
 import ReactDOM from 'react-dom/client';
-// to jest w package.json i jest zainstalowane lokalnie.
-// One razem tworzą całą bibliotekę.
-
 import './index.css';
-// import css'a
-
 import App from './App';
 // import componentu App.
 
